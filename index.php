9af99d89fa298f2aaebe118dbd855f42d169ad29
Marjan Stojanovski <stojanovskimarjan4@gmail.com>
2022-12-18 20:50:07 +0100

    finished post to process.php

--- index.php
+++ index.php
@@ -3,7 +3,69 @@
     require_once "header.php";
 ?>
 
-<h1>Hello, world!</h1>
+<main>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <form class="form" method="post" action="/process/process.php">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="inputEmail3" class="col-sm-2 control-label">First Name</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="inputEmail3" placeholder="First Name" name="firstName">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="inputPassword3" class="col-sm-2 control-label">Last Name</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="inputPassword3" placeholder="Last Name" name="lastName">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control" id="inputEmail3" placeholder="Email" name="email">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+                            <div class="col-sm-10">
+                                <input type="password" class="form-control" id="inputPassword3" placeholder="Password" name="password">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="iagree"> Remember me
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                                <button type="submit" class="btn btn-default">Submit</button>
+                            </div>
+                        </div>
+                    </div>
+            </form>
+            </div>
+        </div>
+    </div>
+
+
+
+</main>
+
 
 <?php
 
---
+++ process/process.php
@@ -0,0 +1,29 @@
+<?php
+
+
+$firstName = '';
+$lastName = '';
+$email = '';
+$password = '';
+
+if(isset($_POST['firstName']) && !empty($_POST['firstName'])) {
+    $firstName = $_POST['firstName'];
+};
+
+if(isset($_POST['lastName']) && !empty($_POST['lastName']))  {
+    $lastName = $_POST['lastName'];
+};
+
+if(isset($_POST['email']) && !empty($_POST['email']))  {
+    $email = $_POST['email'];
+};
+
+if(isset($_POST['password']) && !empty($_POST['password']))  {
+    $password = $_POST['password'];
+};
+
+echo $firstName . " " . $lastName . " " . $email . " " . $password;
+
+
+
+?>
