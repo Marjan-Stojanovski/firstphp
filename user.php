bc81ee4cb1da126663161e911fd602d97d92c408
Marjan Stojanovski <stojanovskimarjan4@gmail.com>
2022-12-19 19:35:30 +0100

    finished get in user.php

---
+++ user.php
@@ -0,0 +1,20 @@
+<?php require_once "header.php";  ?>
+
+<?php
+
+$firstName = $_GET['firstName'];
+$lastName = $_GET['lastName'];
+$email = $_GET['email'];
+
+
+echo $firstName . " " . $lastName . " " . $email . " " . "Od user.php rezultat";
+
+
+?>
+
+
+
+
+
+<?php require_once "footer.php";  ?>
+
